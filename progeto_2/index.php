507b233e239bf98982fe03f94bf131b8a4b68375
leonardo alves <leo.frogeri@gmail.com>
2020-07-09 04:45:19 -0300

    Meu segundo website

---
+++ progeto_2/index.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>progeto_2</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0">
+	<meta name="description" content="descrição do meu site">
+	<meta name="keywords" content="palavras-chaves,separadas,por,virgula">
+	<meta name="author" content="Leonardo alves">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+	<header>
+		<div class="center">
+			
+			<div><!--div-cega-->
+				<div class="logo-left"></div>
+			</div><!--div-cega-->
+			
+			<div><!--div-cega-->
+				<div class="logo-rigth"></div><!--logo2-->
+			</div><!--div-cega-->
+
+		</div><!--center-->
+		<div class="clear"></div><!--center-->
+	</header>
+
+
+	<section class="form-input">
+		<div class="center banner">
+			
+				<div class="container-form">
+					<h2>DEIXE AQUI SEUS DADOS NO FORMULARIO ABAIXO!</h2>
+				<form>
+					<div class="input-container">
+						<p>*NOME</p>
+						<input type="text" name="name" placeholder="NOME" required>
+					</div><!--input-container-->
+
+					<div class="input-container">
+						<p>*E-MAIL</p>
+						<input type="text" name="email" placeholder="E-MAIL" required>
+					</div><!--input-container-->
+
+					<div class="input-container">
+						<p>*SENHA</p>
+						<input type="password" name="senha" placeholder="SENHA" required>
+					</div><!--input-container-->
+
+					<div class="campos">
+						<p>*CAMPOS OBRIGATORIOS</p>
+					</div>
+
+
+					<div class="input-container">
+						<input type="submit" name="ação" value="Enviar">
+					</div><!--input-container-->
+			</form>
+			</div><!--container-form-->
+		<div class="claer"></div>
+		</div><!--center-->
+	</section>
+
+	<div class="row"></div><!--row-->
+
+
+	<section class="container-main">
+		<div class="center fundo">
+			<div class="texto">
+				<h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h2>
+				<h4>vulputate nunc mattis placerat</h4>
+			</div><!--texto-->
+		</div><!--center-->
+	</section><!--container-main-->
+
+	<section class="mosaico">
+			
+			<div class="center">
+				
+				<div class="flex-mosaico">
+					
+					<div style="background-image: url('imagem/mosaico1.jpg');" class="item-1"></div><!--item-1-->
+
+					<div class="item-2">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dictum elit non ipsum egestas, in vehicula ex suscipit. Sed in sodales orci. Morbi aliquet mi ex, vitae vulputate felis ultricies eget. Maecenas a gravida nunc, eget tempus ex. Phasellus nec metus turpis. Sed congue</p>
+					</div><!--item-2-->
+
+				</div><!--flex-mosaico-->
+
+				<div class="flex-mosaico">
+					
+					<div style="background-image: url('imagem/mosaico2.jpg'); order: 2;" class="item-1"></div><!--item-1-->
+
+					<div class="item-2" style="order: 1;">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dictum elit non ipsum egestas, in vehicula ex suscipit. Sed in sodales orci. Morbi aliquet mi ex, vitae vulputate felis ultricies eget. Maecenas a gravida nunc, eget tempus ex. Phasellus nec metus turpis. Sed congue</p>
+					</div><!--item-2-->
+
+				</div><!--flex-mosaico-->
+
+					<div class="flex-mosaico">
+					
+					<div style="background-image: url('imagem/mosaico3.jpg');" class="item-1"></div><!--item-1-->
+
+					<div class="item-2">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dictum elit non ipsum egestas, in vehicula ex suscipit. Sed in sodales orci. Morbi aliquet mi ex, vitae vulputate felis ultricies eget. Maecenas a gravida nunc, eget tempus ex. Phasellus nec metus turpis. Sed congue</p>
+					</div><!--item-2-->
+
+				</div><!--flex-mosaico-->
+
+					<div class="flex-mosaico">
+					
+					<div style="background-image: url('imagem/mosaico4.jpg'); order: 2;" class="item-1"></div><!--item-1-->
+
+					<div class="item-2" style="order: 1;">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dictum elit non ipsum egestas, in vehicula ex suscipit. Sed in sodales orci. Morbi aliquet mi ex, vitae vulputate felis ultricies eget. Maecenas a gravida nunc, eget tempus ex. Phasellus nec metus turpis. Sed congue</p>
+					</div><!--item-2-->
+
+				</div><!--flex-mosaico-->
+
+			</div><!--center-->
+
+	</section><!--mosaico-->
+
+
+	<footer>
+		<div class="center">
+			<div class="footer2">
+				<div class="img">
+					
+					<img src="imagem/logo1.png">
+					
+					<div class="img2">
+						<img src="imagem/logo2.png">
+					</div>
+				
+				</div>
+
+				<div class="container-footer">
+					<h1>NUMERO PARA CONTATO:<a href="#">(11) 9-6888-6688</a></h1>
+					<h1>EMAIL:<a href="#">LEO.FROGERI@GMAIL.COM</a></h1>
+					<p>ras sed mi ipsum. Sed at finibus nisi, in pharetra sapien. Vestibulum quam lorem, venenatis eu nunc vitae, commodo laoreet ipsum. Vivamus a molestie leo. Nam in porttitor risus. Donec quis lectus vehicula, auctor ante ac, fringilla risus. Aenean et .</p>
+				</div>	
+
+				</div>
+		</div>
+		<div class="claer"></div>
+	</footer>
+
+
+</body>
+</html>
